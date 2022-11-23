67e35e7aafa78c34d8766ff6215d6720ab4bdcec
HaiUTC <sahaetran@gmail.com>
2022-11-23 15:44:31 +0700

    make it better

---
+++ server/src/untils/generatedRandomString.ts
@@ -0,0 +1,12 @@
+export const makeid = (length: number) =>{
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+console.log(makeid(5));
